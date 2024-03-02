551b8c28ceabe119e25cb95ac62d34692b4546a9
martic38 <105032725+martic38@users.noreply.github.com>
2024-03-01 17:42:41 -0800

    One like per user update

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ let fromEl = document.getElementById("from-el")
 let toEl = document.getElementById("to-el")
 let endorsementContainerEl = document.getElementById("endorsement-container")
 
+//ocalstorage Init
+let likedIDsByApp = []
+if (JSON.parse(localStorage.getItem("likedIDs"))) {
+    likedIDsByApp = JSON.parse(localStorage.getItem("likedIDs"))
+    
+}
+
 
 //listen for clicks on Publish button
 publishBtnEl.addEventListener('click', addEndorsement)
@@ -28,42 +35,64 @@ document.addEventListener("click", function(e){
     let currentLikeBtn = target.id
     if(target){
       let exactLocationOfItemInDB = ref(database, `endorsementList/${currentLikeBtn}`)
+      
       // DB transaction
       runTransaction(exactLocationOfItemInDB, (endorseObject) => {
-        console.log(endorseObject)
-            if(endorseObject) {
-                if (endorseObject.likeCount) {
-                    endorseObject.likeCount--
+            
+            // if(endorseObject) {
+            //     if (endorseObject.likeCount) {
+            //         endorseObject.likeCount--
                     
-                } else {
-                    endorseObject.likeCount++
-                    if (!endorseObject.likeCount) {
-                        endorseObject.likeCount = {}
-                    }
+            //     } else {
+            //         endorseObject.likeCount++
+            //         if (!endorseObject.likeCount) {
+            //             endorseObject.likeCount = {}
+            //         }
                 
-                }
+            //     }
+            // }
+            if (validateLike(currentLikeBtn)) {
+                endorseObject.likeCount--
+                let indexOfLikedEndorsement = likedIDsByApp.indexOf(currentLikeBtn)
+                likedIDsByApp.splice(indexOfLikedEndorsement, 1)
+                console.log("CANNY LIKE ANYMORE")
+            } else {
+                endorseObject.likeCount++
+                likedIDsByApp.push(currentLikeBtn)
             }
-            // endorseObject.likeCount++
+            localStorage.setItem("likedIDs", JSON.stringify(likedIDsByApp))
         return endorseObject
       })
     }
   });
    
+function validateLike(endorsementID) {
+    if (likedIDsByApp) {
+        return likedIDsByApp.includes(endorsementID)
+    } else {
+            return false
+    }
+}
+
 function addEndorsement() {
+
     let endorsementComment = inputFieldEl.value
     let fromVal = fromEl.value
     let toVal = toEl.value
 
-    let newEntryArray = {
-        comment: endorsementComment, 
-        from: fromVal, 
-        to: toVal, 
-        likeCount: 0,
-        timeStamp: Date.now()
+    if (endorsementComment === '' || fromVal === '' || toVal === ''){
+        alert('Please fill out all input fields before publishing');
+    } else {
+            let newEntryArray = {
+            comment: endorsementComment, 
+            from: fromVal, 
+            to: toVal, 
+            likeCount: 0,
+            timeStamp: Date.now()
+            }
+            
+            push(endorsementsInDB, newEntryArray)
         }
-        // console.log(newEntryArray)
-    push(endorsementsInDB, newEntryArray)
-
     clearInputField()
 }
 
@@ -78,7 +107,6 @@ function clearEndorsementContainer() {
 }
 
 function appendEndorsements(endorsement) {
-    
     let newEl = document.createElement("div")
     newEl.setAttribute("id", "endorse-box")
 
@@ -88,29 +116,20 @@ function appendEndorsements(endorsement) {
     let likeCount = endorsement[1].likeCount
     let endorseID = endorsement[0]
 
-    
     newEl.innerHTML = `
     <h4>To ${toValue}</h4>
     <p>${endorseCommentValue}</p>
     <h4 style="text-align:left;float:left;">From ${fromValue}</h4>
     <button style="text-align:right;float:right;" class="likeEl" id="${endorseID}">♥${likeCount}</button>`
     
-    // let likeEl = document.getElementById("like-el")
-    // console.log(likeEl)
-    // likeEl.addEventListener('click', function(){
-    //     console.log("LIKED")
-    // })
-
     endorsementContainerEl.append(newEl)
 }
 
 //if  any change to the DB happens
 onValue(endorsementsInDB, function(snapshot){
 //grab all entries
- clearEndorsementContainer()
-
+    clearEndorsementContainer()
     let endorsementsArray = Object.entries(snapshot.val()).reverse()
-    //    console.log(endorsementsArray)
     for (let i=0; i < endorsementsArray.length ; i++){
             let currentEndorsement = endorsementsArray[i]
             appendEndorsements(currentEndorsement)
